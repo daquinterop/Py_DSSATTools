e8dd38334e10de3aa4c5d75b47649991548bfc48
Diego Quintero <daquinterop@gmail.com>
2023-01-17 08:01:59 -0600

    Setup updated to newer version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 setuptools.setup(
     name="DSSATTools",
-    version="2.0.2d",
+    version="2.0.3",
     author="Diego Quintero",
     author_email="daquinterop@gmail.com",
     description="A DSSAT's Python implementation",
