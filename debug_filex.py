4b4dd5f6ca4e0fa3e8f3bfb3bde9ed1045ba5018
Diego Quintero <daquinterop@gmail.com>
2024-09-28 10:40:52 -0500

    Switch to main

--- debug_filex.py
+++ debug_filex.py
@@ -4,7 +4,14 @@ from DSSATTools.filex import (
 from DSSATTools.base.partypes import NumberType
 from datetime import date
 
-read_filex("/home/diego/dssat-csm-data/Maize/EBPL8501.MZX")
+# experiment = read_filex("/home/diego/dssat-csm-data/Maize/EBPL8501.MZX")
+experiment = read_filex("/home/diego/dssat-csm-data/Soybean/UFGA8401.SBX")
+field = experiment["Field"][1]
+field._write_section()
+planting = experiment["Planting"][1]
+
+planting._write_section()
+# read_filex("/home/diego/dssat-csm-data/Sunflower/INRA0601.SUX")
 
 ic = InitialConditions("MZ",
     table=[InitialConditionsLayer(10, 0.4,), InitialConditionsLayer(20, 0.4,)])
