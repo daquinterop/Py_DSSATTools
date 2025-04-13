8bab6e4d6040acdb21aa08e8874977df354fdf1c
Diego Quintero <daquinterop@gmail.com>
2025-04-13 19:16:37 +0200

    added docs requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
+    'sphinx_rtd_theme'
 ]
 
 intersphinx_mapping = {
@@ -55,7 +56,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx-rtd-theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
